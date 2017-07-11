bb45b922af6fedcccddd64b9eddfea53ffb5b168
Panda <panda@PandanoiMac.local>
2017-07-11 10:11:14 +0900

    添加UIViewController+Extension

--- MJCompatible/MJCompatible/UIViewController+Extension.swift
+++ MJCompatible/MJCompatible/UIViewController+Extension.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2017年 MooneyWang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public struct UIViewControllerProxy {
+    fileprivate let base: UIViewController
+    init(proxy: UIViewController) {
+        base = proxy
+    }
+}
+
+extension UIViewController: MJCompatible {
+    public typealias CompatibleType = UIViewControllerProxy
+    public var mj: UIViewControllerProxy {
+        return UIViewControllerProxy(proxy: self)
+    }
+}
+
+extension UIViewControllerProxy {
+
+    /// Returns the top most view controller from given view controller's stack.
+    public static func topMost(of viewController: UIViewController?) -> UIViewController? {
+        // UITabBarController
+        if let tabBarController = viewController as? UITabBarController,
+            let selectedViewController = tabBarController.selectedViewController {
+            return self.topMost(of: selectedViewController)
+        }
+
+        // UINavigationController
+        if let navigationController = viewController as? UINavigationController,
+            let visibleViewController = navigationController.visibleViewController {
+            return self.topMost(of: visibleViewController)
+        }
+
+        // presented view controller
+        if let presentedViewController = viewController?.presentedViewController {
+            return self.topMost(of: presentedViewController)
+        }
+
+        // child view controller
+        for subview in viewController?.view?.subviews ?? [] {
+            if let childViewController = subview.next as? UIViewController {
+                return self.topMost(of: childViewController)
+            }
+        }
+        
+        return viewController
+    }
+}
--- MJCompatible/ViewController.swift
+++ MJCompatible/ViewController.swift
@@ -17,6 +17,18 @@ class ViewController: UIViewController {
         let url: NSString = "http://www.baidu.com"
         print(url.substring(to: 4))
         print(name.mj.substring(from: 7, to: 10))
+
+        let tableVC = UITableViewController()
+        tableVC.view.frame = CGRect(x: 0, y: 0, width: 320, height: 667)
+        self.view.addSubview(tableVC.view)
+        self.addChildViewController(tableVC)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        if let vc = UIViewControllerProxy.topMost(of: self) {
+            print(vc)
+        }
     }
 }
 
